fda5647a04d8efe3f4cb2cc40a2ed13c0d9c9ec0
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2023-07-09 21:52:59 -0400

    ⚡corrected git issues | pushing PR..

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,43 @@
 import { NextPage } from "next";
 // _______________________________________________
 
-// type HomeProps = {
-// 	mockProp?: string;
-// };
+const lineStyle = {
+	borderTop: '4px solid mediumpurple',
+	width: '100%',
+	margin: '0.5rem 0',
+} as const;
+
+const customStyles = {
+	backgroundColor: '#2f323a',
+	margin: '5rem auto',
+	padding: '0 5%',
+	width: 'fit-content',
+	borderRadius: '15px',
+} as const;
+
+const wrapperStyle = {
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'stretch', // This will make the wrapper div as wide as the customStyles div
+	width: '100%',
+} as const;
+
+const h1Style = {
+	padding: '10px',
+	fontFamily: 'Liberation Mono for Powerline',
+	color: 'mediumpurple',
+	fontSize: '1.7rem',
+	display: 'grid',
+	placeItems: 'center',
+} as const;
 // _______________________________________________
 
-const HomePage: NextPage = () => {
+type HomeProps = {
+	mockProp?: string;
+};
+// _______________________________________________
+
+const HomePage: NextPage<HomeProps> = ({ mockProp = 'HomePage' }) => {
 	
 	// _________________ [functions] ___________________
 	
@@ -18,7 +49,13 @@ const HomePage: NextPage = () => {
 	return (
 		//==== <>fragments</> ====
 		<main>
-			<div>Home Page</div>
+			<div style={ customStyles }>
+				<div style={ wrapperStyle }>
+					<div style={ lineStyle } />
+					<h1 style={ h1Style }>@{ mockProp }</h1>
+					<div style={ lineStyle } />
+				</div>
+			</div>
 		</main>
 	);
 };
